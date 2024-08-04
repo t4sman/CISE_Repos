87228e700c15a0babb3990458a9415ebcd7f144e
t4sman <wvz8937@autuni.ac.nz>
2024-08-04 16:49:03 +1200

    added tests

--- worksheet 2/my-nest-app/src/main.ts
+++ worksheet 2/my-nest-app/src/main.ts
@@ -8,6 +8,6 @@ const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(PORT || 5000);
 }
 bootstrap();
